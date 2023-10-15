892729c2eaf10be7da74cbabd43cf7cd57bef8cd
Tat00n <wemanzanero@gmail.com>
2023-10-15 14:17:19 -0600

    Add units to Ingredient model

--- inventory/models.py
+++ inventory/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.CharField(max_length=255, unique=True)
     quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    unit = models.CharField(max_length=50, default="Unit") # Example values: "tbsp", "lbs", etc.
     price_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
