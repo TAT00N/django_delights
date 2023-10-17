99135409e91c0362343c0f16f127dda9c1d711cc
Tat00n <wemanzanero@gmail.com>
2023-10-16 23:09:54 -0600

    Update the admin, and connected menuitem with ingredients

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
+from .models import Ingredient, MenuItem, MenuItemIngredient, Purchase
 # Register your models here.
 
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'quantity', 'unit', 'price_per_unit')
     search_fields = ['name']
 
+class MenuItemIngredientInline(admin.TabularInline):
+    model = MenuItemIngredient
+    extra = 1
+
+class MenuItemAdmin(admin.ModelAdmin):
+    inlines = [MenuItemIngredientInline]
+
 admin.site.register(Ingredient, IngredientAdmin)
-admin.site.register(MenuItem)
-admin.site.register(RecipeRequirement)
 admin.site.register(Purchase)
+admin.site.register(MenuItem, MenuItemAdmin)
